ba2e34080d4ed7f64e12d7942aa259fc8dd9b6fd
charan1147 <charanc1147@gmail.com>
2025-07-18 12:36:12 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB=async ()=>{
-    try {
-       await mongoose.connect(process.env.MONGO_URI)
-        console.log("mongodb is conneceted");
-        
-    } catch (error) {
-        console.error(`Error while connecting DataBase`,error.message);
-        process.exit(1)
-        
-        
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongodb is connected");
+  } catch (error) {
+    console.error(`Error while connecting DataBase`, error.message);
+    process.exit(1);
+  }
+};
 
-    
-}
-
-export default connectDB
+export default connectDB;
