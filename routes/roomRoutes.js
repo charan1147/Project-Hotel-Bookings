854744ac9ac2830173e95e7d5c4731ebb470d94b
charan1147 <charanc1147@gmail.com>
2026-02-10 08:15:08 +0530

    final repo

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -2,19 +2,19 @@ import express from "express";
 import auth from "../middleware/authMiddleware.js";
 import admin from "../middleware/checkRole.js";
 import {
-  createRooms,
-  getRooms,
-  getRoom,
+  createRoom,
+  getRoomById,
+  getAllRooms,
   updateRoom,
   deleteRoom,
 } from "../controllers/roomController.js";
 
 const router = express.Router();
 
-router.get("/", auth, getRooms);
-router.get("/:id", auth, getRoom);
+router.get("/", auth, getAllRooms);
+router.get("/:id", auth, getRoomById);
 
-router.post("/create", auth, admin, createRooms);
+router.post("/create", auth, admin, createRoom);
 router.put("/:id", auth, admin, updateRoom);
 router.delete("/:id", auth, admin, deleteRoom);
 
